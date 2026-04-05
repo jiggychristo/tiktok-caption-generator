b42778e55c217750617d764e4a52763bbb141c59
jiggychristo <ngchristopherson@gmail.com>
2026-04-05 17:06:22 -0500

    pwa, dark mode, swipe, api key memory, reroll, analyze, notes, counter

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "caption-gen-v1";
+const ASSETS = [
+  "/tiktok-caption-generator/",
+  "/tiktok-caption-generator/index.html"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
